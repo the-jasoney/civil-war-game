9a909177a8fcb8ba6b80141ab170bb8b5089506a
the-jasoney <jasoney.music@gmail.com>
2024-05-27 22:33:53 -0700

    improve instructions

--- src/components/mainmenu/MainMenu.tsx
+++ src/components/mainmenu/MainMenu.tsx
@@ -1,4 +1,6 @@
+import { IoMdStopwatch } from 'react-icons/io'
 import './MainMenu.scss'
+import { FaTrophy } from 'react-icons/fa'
 
 interface MainMenuProps {
     hidden: boolean,
@@ -15,10 +17,10 @@ export function MainMenu({ hidden, startGame }: MainMenuProps) {
                     <em>Note: This game works best when run in fullscreen mode (</em> <kbd>F11</kbd> <em>or</em> <kbd>Fn</kbd>+<kbd>F11</kbd> <em>) on most browsers and keyboards</em>
                     <br />
                     <br />
-                    You are given 30 seconds (shown after timer icon or in the top progress bar) to answer as much questions as possible. For every question, you get troops, shown on the right after the Union flag.
+                    You are given 30 seconds (shown after <IoMdStopwatch width="25px" height="25px" /> or in the top progress bar) to answer as much questions as possible. For every question, you get troops, shown on the right after the Union flag.
                     <br />
                     <br />
-                    Every 30 seconds, the confederates will attack, with increasing numbers every wave (confederate numbers shown after confederate flag on the right, and waves shown after the trophy).
+                    Every 30 seconds, the confederates will attack, with increasing numbers every wave (confederate numbers shown after the confederate flag on the right, and waves shown after the <FaTrophy width="25px" height="25px" />).
                     <br />
                     <br />
                     If the confederate soldiers make it to Washington, D.C., they decrease the health, shown as the top red bar. If the health reaches 0, the game is over.
